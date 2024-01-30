fa6f987f353e94332300e98188b907aa5df0b01c
fengyuanliang3 <fengyuanliang2@jd.com>
2024-01-31 00:39:27 +0800

    chore: text/event-stream

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -34,38 +34,70 @@ export const parseOpenAIStream = (rawResponse: Response) => {
 
   const stream = new ReadableStream({
     async start(controller) {
-      const streamParser = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === 'event') {
-          const data = event.data
-          if (data === '[DONE]') {
-            controller.close()
-            return
-          }
-          try {
-            // response = {
-            //   id: 'chatcmpl-6pULPSegWhFgi0XQ1DtgA3zTa1WR6',
-            //   object: 'chat.completion.chunk',
-            //   created: 1677729391,
-            //   model: 'gpt-3.5-turbo-0301',
-            //   choices: [
-            //     { delta: { content: '你' }, index: 0, finish_reason: null }
-            //   ],
-            // }
-            const json = JSON.parse(data)
-            const text = json.choices[0].delta?.content || ''
-            const queue = encoder.encode(text)
-            controller.enqueue(queue)
-          } catch (e) {
-            controller.error(e)
-          }
+      for await (const chunk of rawResponse.body as any) {
+        const data = decoder.decode(chunk).split('data: ')[1]
+        if (data === '[DONE]') return
+        try {
+          // console.log(data)
+          // response = {
+          //   id: 'chatcmpl-6pULPSegWhFgi0XQ1DtgA3zTa1WR6',
+          //   object: 'chat.completion.chunk',
+          //   created: 1677729391,
+          //   model: 'gpt-3.5-turbo-0301',
+          //   choices: [
+          //     { delta: { content: '你' }, index: 0, finish_reason: null }
+          //   ],
+          // }
+
+          const json = JSON.parse(data)
+          const text = json.choices[0].delta?.content || ''
+          const queue = encoder.encode(text)
+          controller.enqueue(queue)
+        } catch (e) {
+          controller.error(e)
         }
       }
 
-      const parser = createParser(streamParser)
-      for await (const chunk of rawResponse.body as any)
-        parser.feed(decoder.decode(chunk))
+      controller.close()
+
+      // const streamParser = (event: ParsedEvent | ReconnectInterval) => {
+      //   if (event.type === 'event') {
+      //     const data = event.data
+      //     if (data === '[DONE]') {
+      //       controller.close()
+      //       return
+      //     }
+      //     try {
+      //       // response = {
+      //       //   id: 'chatcmpl-6pULPSegWhFgi0XQ1DtgA3zTa1WR6',
+      //       //   object: 'chat.completion.chunk',
+      //       //   created: 1677729391,
+      //       //   model: 'gpt-3.5-turbo-0301',
+      //       //   choices: [
+      //       //     { delta: { content: '你' }, index: 0, finish_reason: null }
+      //       //   ],
+      //       // }
+      //       const json = JSON.parse(data)
+      //       const text = json.choices[0].delta?.content || ''
+      //       const queue = encoder.encode(text)
+      //       controller.enqueue(queue)
+      //     } catch (e) {
+      //       controller.error(e)
+      //     }
+      //   }
+      // }
+
+      // const parser = createParser(streamParser)
+      // for await (const chunk of rawResponse.body as any)
+      //   parser.feed(decoder.decode(chunk))
     },
   })
 
-  return new Response(stream)
+  const response = new Response(stream)
+
+  response.headers.set('Content-Type', 'text/event-stream')
+  response.headers.set('Cache-Control', 'no-cache')
+  response.headers.set('Connection', 'keep-alive')
+
+  return response
 }
